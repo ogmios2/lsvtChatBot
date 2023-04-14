eec5b85e7a7a4f17c5e76dfc61a54ff9a76cafd5
Olivier Cuny <ogmios2@gmail.com>
2023-04-14 10:04:28 -0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,102 @@
+import pinecone
+import promptlayer
+import os
+import gradio as gr
+import re
+
+from langchain.vectorstores import Pinecone
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import PromptLayerChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts import PromptTemplate
+
+
+# LOCAL STUFF
+# Set Key Variables
+from dotenv import load_dotenv
+load_dotenv()
+promptlayer.api_key = os.getenv("PROMPTLAYER_API_KEY")
+pinecone.api_key = os.getenv("PINECONE_API_KEY")
+# END LOCAL STUFF
+
+# Prompt Template & Messages
+_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:"""
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+template = """Based on the context provided, provide an answer to the best of your knowledge.
+Use your skills to determine what kind of context is provided and tailor your response accordingly. 
+When providing an answer, choose the tone of voice and humor of Zapp Brannigan from Futurama. Also, use html bullet list format when needed.
+Question: {question}
+=========
+{context}
+=========
+"""
+QA_PROMPT = PromptTemplate(template=template, input_variables=["question", "context"])
+
+# Initialize Pinecone
+pinecone.init(
+    environment="us-central1-gcp"
+)
+index_name = "langchain-demo1"
+
+# Replace kb_db_store initialization with Pinecone.from_existing_index method
+embeddings = OpenAIEmbeddings()
+kb_db = Pinecone.from_existing_index(index_name=index_name, embedding=embeddings, namespace="lsvt")
+
+
+# Third, we need to create the prompt
+# Initialize the ChatVectorDBChain
+kb_chat = ConversationalRetrievalChain.from_llm(
+    PromptLayerChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", pl_tags=["local", "customTemp7"]),
+    retriever=kb_db.as_retriever(search_kwargs={"k": 3}),    
+    verbose=True, 
+    return_source_documents=True,
+    qa_prompt=QA_PROMPT,
+    condense_question_prompt=CONDENSE_QUESTION_PROMPT,
+)
+
+# Format the text in the return
+def format_terms(text):
+    formatted_text = re.sub(r'[“"”]([^”“]+)[“"”]', r'<b>\1</b>', text)
+    formatted_text = formatted_text.replace('\n', '<br>')
+    return formatted_text
+
+
+chat_history = []
+def get_answer(query):
+    
+    result = kb_chat({"question": query, "chat_history": chat_history})
+    chat_history.append((query, result["answer"])) 
+
+     # Use a set to remove duplicates
+    source_urls = list(set(doc.metadata["source"] for doc in result["source_documents"]))  
+    
+    formatted_answer = format_terms(result['answer'])    
+    
+    answer = f"<strong>Answer:</strong><br><br> {formatted_answer}<br><br><strong>Source URLs:</strong><br>"
+    answer += "<br>".join(f'<a href="{url}" target="_blank">{url}</a>' for url in source_urls)
+    
+    return answer
+
+# Local Interface
+examples = [
+    ["what was one of the first things the president do?"],
+    ["why did he do it?"]
+]
+
+# Define the input and output components for the Gradio interface
+input_query = gr.inputs.Textbox(lines=2, label="Enter a question:")
+output_answer = gr.outputs.HTML(label="Answer:")
+
+# Launch the Gradio interface with the function and components
+demo = gr.Interface(fn=get_answer, 
+                    inputs=input_query, 
+                    outputs=output_answer, 
+                    title="Support Site Chat Bot", 
+                    allow_flagging="never",
+                    examples=examples
+                    )
+demo.launch()
---
+++ ingest.py
@@ -0,0 +1,44 @@
+import pinecone
+
+from langchain.vectorstores import Pinecone
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.text_splitter import TokenTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+
+# LOCAL STUFF
+# Set Key Variables
+from dotenv import load_dotenv
+load_dotenv()
+pinecone.api_key = os.getenv("PINECONE_API_KEY")
+# END LOCAL STUFF
+
+"""
+Pinecone Setup
+"""
+# Initialize pinecone
+pinecone.init(
+    environment="us-central1-gcp"  # next to api key in console
+)
+index_name = "langchain-demo1"
+"""
+END Pinecone Setup
+"""
+
+# First, we need to initialize load URLs and load the text
+# Opens the file and reads the URLs
+with open('./urls.txt', 'r') as file:
+        urls = [line.strip() for line in file]
+
+# Loads the list of URLS and all the text (Consider using Selenium URL loader)
+loader = UnstructuredURLLoader(urls=urls)
+kb_data = loader.load()
+
+# Second, split the text into chunks, using Tiktoken
+text_splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=100)
+kb_docs = text_splitter.split_documents(kb_data) #Note that split_documents works but not split_text because we're dealing with a list of documents
+
+# Create the embeddings
+embeddings = OpenAIEmbeddings()
+
+# Store in the DB
+kb_db_store = Pinecone.from_documents(kb_docs, embeddings, index_name=index_name, namespace="lsvt")
